03b47d17d42efc331df7b41d38598cc68ef8b54e
James A. Overton <james@overton.ca>
2020-10-27 15:25:24 -0400

    Bump to v0.1.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description="Gizmos for ontology development",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.1.3",
+    version="0.1.4",
     author="James A Overton",
     author_email="james@overton.ca",
     classifiers=[
