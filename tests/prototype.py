bc3d46132d7b81857f74895d9dfaafef91560083
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-05-09 12:33:08 -0400

    fix typo

--- tests/prototype.py
+++ tests/prototype.py
@@ -310,7 +310,7 @@ def thick2obj(thick_row):
             if 'annotations' in thick_row:
                 triples += predicateMap2triples(decompress_annotation(target, 'value'))
             if 'metadata' in thick_row:
-                triples += predicat(decompress_reification(target, 'value'))
+                triples += predicate(decompress_reification(target, 'value'))
 
     return target if not triples else triples
 
