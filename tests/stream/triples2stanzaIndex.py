e8824f1b55ab487f7b49476642c6c4e4e99a616d
chris <chris.kind.man@gmail.com>
2021-05-27 12:23:15 +0100

    Add documentation comments

--- tests/stream/triples2stanzaIndex.py
+++ tests/stream/triples2stanzaIndex.py
@@ -20,9 +20,10 @@ def isBlankNode(o):
 
 
 def triples2index(triples):
-    """Converts a triple store into an index system where
-    each subject is assigned a unique ID and
-    triples with the same subject are stored in a file named after the corresponding ID."""
+    """Converts a triple store into an index system. 
+    - Each subject is assigned a unique ID 
+    - Triples with the same subject are stored in a file named after the corresponding ID
+    - The file 'subject2id' specifies the mapping from subjects to the index id."""
 
     path = os.getcwd() + "/index"
     os.mkdir(path) 
@@ -59,8 +60,9 @@ def triples2index(triples):
     return subject2id
 
 def getSubject2indexMap(index):
-    """Takes a path 'index' to indexed triple store and
-    collectes all dependencies of indexed subjects in a 'dependency' file"""
+    """Takes a path 'index' to an indexed of triple store. 
+    Returns a map as specified by the subject2id file of the index."""
+
 
     #get subject2id map from index 
     subject2id = {}
@@ -74,6 +76,9 @@ def getSubject2indexMap(index):
     return subject2id
 
 def getDependencies(index, subject, subject2id):
+    """Takes a path 'index' to an indexed of triple store and a subject.
+    Returns all (transitive) blank node dependencies of the subject as a set.
+    """
     dependencies = set()
     toVisit = set()
     toVisit.add(subject2id[subject])
@@ -92,6 +97,9 @@ def getDependencies(index, subject, subject2id):
     return dependencies 
 
 def getSubject2type(index):
+    """Takes a path 'index' to an indexed of triple store. 
+    Returns a map from subjects to a list of their rdf:types.
+    """
     subject2id = getSubject2indexMap(index)
     subject2types = {}
     for s, id in subject2id.items():
@@ -99,6 +107,7 @@ def getSubject2type(index):
                 triples = list(csv.DictReader(fh, delimiter=" "))
                 subject2types[s] = []
                 for t in triples: 
+                    #TODO: commit to abbreviated namespeces, i.e. 'rdf:type'?
                     if(t["predicate"] == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>" or t["predicate"] == "rdf:type"):
                         subject2types[s].append(t["object"])
 
@@ -144,6 +153,8 @@ def index2stanza(index):
 
 #TODO: include blank nodes that are associated with disjoint classes, etc..
 def getRootSubjects(subject2id):
+    """Takes a path to an 'index' and returns a set of all root subjects.
+    (A root subject is a subject that can be associated with an OWL axiom or the subject of a thick triple."""
     for s, id in subject2id.items():
         if(not isBlankNode(s)):
             print(s + ":" + str(id))
