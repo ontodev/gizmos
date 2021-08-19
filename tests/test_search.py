796278fe16f3fc5b4fd0a6a3185a4f8d4893d1a2
Rebecca Jackson <rbca.jackson@gmail.com>
2021-08-19 10:51:58 -0700

    Add mixed case search to tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -10,6 +10,13 @@ def search_text(conn):
     assert res == expected
 
 
+def search_text_mixed_case(conn):
+    res = search(conn, "buFFeR").strip()
+    with open("tests/resources/obi-search.json", "r") as f:
+        expected = f.read().strip()
+    assert res == expected
+
+
 def search_text_with_options(conn):
     res = search(conn, "buffer", short_label="ID", synonyms=["IAO:0000118"]).strip()
     with open("tests/resources/obi-search-options.json", "r") as f:
@@ -21,9 +28,13 @@ def test_search_postgresql(create_postgresql_db):
     engine = create_engine(postgres_url)
     with engine.connect() as conn:
         search_text(conn)
+        search_text_mixed_case(conn)
+        search_text_with_options(conn)
 
 
 def test_search_sqlite(create_sqlite_db):
     engine = create_engine(sqlite_url)
     with engine.connect() as conn:
         search_text(conn)
+        search_text_mixed_case(conn)
+        search_text_with_options(conn)
